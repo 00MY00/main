7ea4297c4a218a04cc5c4c123678cf751539439e
00MY00 <75865145+00MY00@users.noreply.github.com>
2021-12-21 17:16:01 +0100

    Add files via upload

---
+++ bienvenu.bat
@@ -0,0 +1,47 @@
+@echo off
+color e
+chcp 65001
+mode con cols=40 lines=40
+title BIENVENU
+
+set back=%~dp0
+
+set back=%back:~0,-5%
+echo.
+echo.
+
+echo             [40;32mBIENVENU [40;31m%USERNAME% 
+echo [40;33mJe vous remerci pour votre instalation
+echo l'interface va changer 
+echo et les fichier seron instalé
+echo.
+echo.[43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m 
+echo.
+echo [40;36men continuent et en utilisent 
+echo se programe vous 
+echo sertifier etre dacord avec les terme 
+echo de condition
+echo vous empechant de modifier d'une quel 
+echo conque manière 
+echo se programe et les scripts qui i sont 
+echo relier 
+echo il vous est [40;31mINTERDIT 
+echo [40;36mde vandre ou de fair 
+echo du profit sur ce programe sens 
+echo autorisation du 
+echo propriétaire
+echo.
+echo.[43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m [43;37m 
+echo.
+echo.[44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m 
+echo.
+echo [40;33mCrée par [40;36mLast-Help[40;36m / [40;32mKuroakashiro[40;36m
+echo éditer par [40;35m00MY00 [40;33m
+echo signier [40;31mA[40;37m.[40;31mI[40;37m.[40;31mB[40;36m
+echo.
+echo.[44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m [44;37m 
+echo.[40;36m
+echo.
+echo.
+pause
+exit
